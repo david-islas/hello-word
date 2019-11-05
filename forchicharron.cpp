da638e6f302dc19ee06d37b0464e351287870a1c
david-islas <57227847+david-islas@users.noreply.github.com>
2019-11-05 10:16:50 -0600

    Add files via upload

---
+++ forchicharron.cpp
@@ -0,0 +1,24 @@
+/*
+	------------------------------------------------------------------
+	|*Universidad Politecnica de Tulancingo                          |
+	|*Ingenieria en Electronica y Telecuminicaciones                 |
+	|*Logica de Programacion                                         |
+	|*David Islas Alcantara                                          |
+	|*programa para hacer fibonacci                                  |
+	------------------------------------------------------------------
+*/
+
+
+#include "pch.h"
+
+int main(){
+	float  a, b, c;
+	printf("capturar a: ");
+	scanf_s("%f", &a);
+	printf("capturar b: ");
+	scanf_s("%f", &b);
+	printf("capturar c: ");
+	scanf_s("%f", &c);
+	printf("valores capturados %f, %f, %f\n", a, b, c);
+	printf("en las direcciones %p,%p,%p\n", &a, &b, &c);
+}
